523c63c92c7f82757d30941aae717e710535298e
Unknown <1780089G@student.tp.edu.sg>
2018-01-17 20:47:39 +0800

    1stcomm

---
+++ forloops.js
@@ -0,0 +1,14 @@
+console.log("Printing all numbers between -10 and 19")
+for(var i = -10; i < 19; i++){
+  console.log(i)
+}
+
+
+
+
+console.log("Printing all even between 10 and 40")
+for(var i = 10; i <= 40; i += 1){
+  if(i % 2 === 0){
+    console.log(i);
+  }
+}
